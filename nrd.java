10dff66657ab25d7623f4036a99a3cf0c36344c7
Vignesh Kumar E <vicky18091997@gmail.com>
2018-08-04 15:08:24 +0530

    Update nrd.java

--- nrd.java
+++ nrd.java
@@ -4,11 +4,14 @@ class nrd
 {
 public static void main(String args[])
 {
+ //getting the input using Scanner
 Scanner sc= new Scanner(System.in);
 int input = sc.nextInt();
 int count=0;
+ //To convert the given number into an array element
 ArrayList<Integer> list = new ArrayList<Integer>();
 
+ //To add the digits to the list
 while(input>0)
 {
 list.add(input%10);
@@ -17,6 +20,7 @@ input=input/10;
 
 for(Integer i: list)
 {
+ //to check for the frequency
 int x= Collections.frequency(list,i);
 if(x==1)
 count++;
